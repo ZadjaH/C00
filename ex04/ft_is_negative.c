bccb9a6c17ad56f0752cd20c80c6c9ad1195d1aa
calcifer <cazinlea0@gmail.com>
2024-03-16 21:18:57 +0100

    init ex04

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calcifer <calcifer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 20:19:27 by calcifer          #+#    #+#             */
+/*   Updated: 2024/03/16 21:15:26 by calcifer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	if (n >= 0)
+		write(1, "P", 1);
+	else
+		write(1, "N", 1);
+}
+
+int	main(void)
+{
+	ft_is_negative(1);
+	ft_is_negative(0);
+	ft_is_negative(-1);
+	return (0);
+}
