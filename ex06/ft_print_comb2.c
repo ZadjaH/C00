094547b10e0dc1208b47f418e86052fcbf41e0a0
calcifer <cazinlea0@gmail.com>
2024-03-17 02:31:42 +0100

    init ex06

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: calcifer <calcifer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 01:44:24 by calcifer          #+#    #+#             */
+/*   Updated: 2024/03/17 02:29:46 by calcifer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_number(int n)
+{
+	char	digit;
+
+	digit = (n / 10) + '0';
+	write(1, &digit, 1);
+	digit = (n % 10) + '0';
+	write(1, &digit, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	n1;
+	int	n2;
+
+	n1 = 0;
+	n2 = 0;
+	while (n1 < 99)
+	{
+		if (n2 > n1)
+		{
+			ft_print_number(n1);
+			write(1, " ", 1);
+			ft_print_number(n2);
+			if (n1 < 98)
+				write(1, ", ", 2);
+		}
+		n2++;
+		if (n2 > 99)
+		{
+			n1++;
+			n2 = n1 + 1;
+		}
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+	ft_print_number(999);
+	write(1, "\n", 1);
+	return (0);
+}
